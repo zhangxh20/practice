1055f5484d66c19add264ba3f75d097f79c3c85e
one <zhangxh20@163.com>
2021-04-24 23:19:54 +0800

    update

--- spring-boot-dubbo/dubbo-service/src/main/java/org/dubbo/service/impl/UserServiceImpl.java
+++ spring-boot-dubbo/dubbo-service/src/main/java/org/dubbo/service/impl/UserServiceImpl.java
@@ -2,9 +2,11 @@ package org.dubbo.service.impl;
 
 import org.apache.dubbo.config.annotation.DubboService;
 import org.dubbo.dao.UserMapper;
+import org.dubbo.entity.User;
 import org.dubbo.request.UserReq;
 import org.dubbo.response.UserResp;
 import org.dubbo.service.UserService;
+import org.springframework.beans.BeanUtils;
 
 import javax.annotation.Resource;
 
@@ -16,11 +18,16 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public void register(UserReq user) {
-
+        User u = new User();
+        BeanUtils.copyProperties(user, u);
+        userMapper.insert(u);
     }
 
     @Override
     public UserResp getUserById(int id) {
-        return null;
+        User user = userMapper.selectById(id);
+        UserResp resp = new UserResp();
+        BeanUtils.copyProperties(user, resp);
+        return resp;
     }
 }
